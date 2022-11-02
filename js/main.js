772432edf998aab5d30e3758c8c446b817d8c733
JasubiPL <gameportal7@hotmail.com>
2022-11-01 22:12:24 -0600

    Se añadio la presentacion

--- js/main.js
+++ js/main.js
@@ -16,38 +16,43 @@ btnProyects.addEventListener("click", proyectsHover);
 
 function homeHover(){
     btnHome.setAttribute("src", "img/icons/home-hover.png");
-    btnAbout.setAttribute("src", "img/icons/About-me.svg");
-    btnBook.setAttribute("src", "img/icons/book.svg");
-    btnWork.setAttribute("src", "img/icons/work.svg");
-    btnProyects.setAttribute("src", "img/icons/proyects.svg");
+    btnAbout.setAttribute("src", "img/icons/About-me.png");
+    btnBook.setAttribute("src", "img/icons/book.png");
+    btnWork.setAttribute("src", "img/icons/work.png");
+    btnProyects.setAttribute("src", "img/icons/proyects.png");
 
 }
 function aboutHover(){
-    btnHome.setAttribute("src", "img/icons/home.svg");
+    btnHome.setAttribute("src", "img/icons/home.png");
     btnAbout.setAttribute("src", "img/icons/About-me-hover.png");
-    btnBook.setAttribute("src", "img/icons/book.svg");
-    btnWork.setAttribute("src", "img/icons/work.svg");
-    btnProyects.setAttribute("src", "img/icons/proyects.svg");
+    btnBook.setAttribute("src", "img/icons/book.png");
+    btnWork.setAttribute("src", "img/icons/work.png");
+    btnProyects.setAttribute("src", "img/icons/proyects.png");
 }
 function bookHover(){
-    btnHome.setAttribute("src", "img/icons/home.svg");
-    btnAbout.setAttribute("src", "img/icons/About-me.svg");
+    btnHome.setAttribute("src", "img/icons/home.png");
+    btnAbout.setAttribute("src", "img/icons/About-me.png");
     btnBook.setAttribute("src", "img/icons/book-hover.png");
-    btnWork.setAttribute("src", "img/icons/work.svg");
-    btnProyects.setAttribute("src", "img/icons/proyects.svg");
+    btnWork.setAttribute("src", "img/icons/work.png");
+    btnProyects.setAttribute("src", "img/icons/proyects.png");
 }
 function workHover(){
-    btnHome.setAttribute("src", "img/icons/home.svg");
-    btnAbout.setAttribute("src", "img/icons/About-me.svg");
-    btnBook.setAttribute("src", "img/icons/book.svg");
+    btnHome.setAttribute("src", "img/icons/home.png");
+    btnAbout.setAttribute("src", "img/icons/About-me.png");
+    btnBook.setAttribute("src", "img/icons/book.png");
     btnWork.setAttribute("src", "img/icons/work-hover.png");
-    btnProyects.setAttribute("src", "img/icons/proyects.svg");
+    btnProyects.setAttribute("src", "img/icons/proyects.png");
 }
 function proyectsHover(){
-    btnHome.setAttribute("src", "img/icons/home.svg");
-    btnAbout.setAttribute("src", "img/icons/About-me.svg");
-    btnBook.setAttribute("src", "img/icons/book.svg");
-    btnWork.setAttribute("src", "img/icons/work.svg");
+    btnHome.setAttribute("src", "img/icons/home.png");
+    btnAbout.setAttribute("src", "img/icons/About-me.png");
+    btnBook.setAttribute("src", "img/icons/book.png");
+    btnWork.setAttribute("src", "img/icons/work.png");
     btnProyects.setAttribute("src", "img/icons/proyects-hover.png");
 }
 
+/*----Banner Video----*/
+let video = document.querySelector(".bannerVideo ");
+setTimeout(()=>{video.style.opacity = "1"},1000);
+let baner = document.querySelector(".contentainerBannerInfo");
+setTimeout(()=>{baner.style.opacity = "1"},2000);
