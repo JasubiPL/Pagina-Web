c79bd5a5321fb50072bb158ad8fe5c13fd5d909a
JasubiPL <gameportal7@hotmail.com>
2022-11-04 00:53:20 -0600

    Se Crerealiza la zona Abaout me y se cambia el diseño de el cursor

--- js/main.js
+++ js/main.js
@@ -1,55 +1,62 @@
 
 //alert("Conectado");
-let btnHome = document.getElementById("btnHome");
-let btnAbout = document.getElementById("btnAbout");
-let btnBook = document.getElementById("btnBook");
-let btnWork = document.getElementById("btnWork");
-let btnProyects = document.getElementById("btnProyects");
-btnHome.addEventListener("click", homeHover);
-btnAbout.addEventListener("click", aboutHover);
-btnBook.addEventListener("click", bookHover);
-btnWork.addEventListener("click", workHover);
-btnProyects.addEventListener("click", proyectsHover);
+const iconNav = document.querySelectorAll(".iconNav");
+
+function defaultIcons(){
+    for(let ico of iconNav){
+        if(ico.classList.contains("btnHome")){
+            ico.setAttribute("src","./img/icons/home.png");
+           }
+           if(ico.classList.contains("btnAbout")){
+            ico.setAttribute("src","./img/icons/About-me.png");
+           }
+           if(ico.classList.contains("btnBook")){
+            ico.setAttribute("src","./img/icons/book.png");
+           }
+           if(ico.classList.contains("btnWork")){
+            ico.setAttribute("src","./img/icons/work.png");
+           }
+           if(ico.classList.contains("btnProyects")){
+            ico.setAttribute("src","./img/icons/Proyects.png");
+           }
+    }
+}
 
 
-/*----Hover nav----*/
+const cambiarIcono = function (){
+   
+   if(this.classList.contains("btnHome")){
+    defaultIcons(); 
+    this.setAttribute("src","./img/icons/home-hover.png");
+    return;
+   }
+   if(this.classList.contains("btnAbout")){
+    defaultIcons();
+    this.setAttribute("src","./img/icons/About-me-hover.png");
+    return;
+   }
+   if(this.classList.contains("btnBook")){
+    defaultIcons();
+    this.setAttribute("src","./img/icons/book-hover.png");
+    return;
+   }
+   if(this.classList.contains("btnWork")){
+    defaultIcons();
+    this.setAttribute("src","./img/icons/work-hover.png");
+    return;
+   }
+   if(this.classList.contains("btnProyects")){
+    defaultIcons();
+    this.setAttribute("src","./img/icons/Proyects-hover.png");
+    return;
+   }
+   
+   
+}
 
-function homeHover(){
-    btnHome.setAttribute("src", "img/icons/home-hover.png");
-    btnAbout.setAttribute("src", "img/icons/About-me.png");
-    btnBook.setAttribute("src", "img/icons/book.png");
-    btnWork.setAttribute("src", "img/icons/work.png");
-    btnProyects.setAttribute("src", "img/icons/proyects.png");
-
-}
-function aboutHover(){
-    btnHome.setAttribute("src", "img/icons/home.png");
-    btnAbout.setAttribute("src", "img/icons/About-me-hover.png");
-    btnBook.setAttribute("src", "img/icons/book.png");
-    btnWork.setAttribute("src", "img/icons/work.png");
-    btnProyects.setAttribute("src", "img/icons/proyects.png");
-}
-function bookHover(){
-    btnHome.setAttribute("src", "img/icons/home.png");
-    btnAbout.setAttribute("src", "img/icons/About-me.png");
-    btnBook.setAttribute("src", "img/icons/book-hover.png");
-    btnWork.setAttribute("src", "img/icons/work.png");
-    btnProyects.setAttribute("src", "img/icons/proyects.png");
-}
-function workHover(){
-    btnHome.setAttribute("src", "img/icons/home.png");
-    btnAbout.setAttribute("src", "img/icons/About-me.png");
-    btnBook.setAttribute("src", "img/icons/book.png");
-    btnWork.setAttribute("src", "img/icons/work-hover.png");
-    btnProyects.setAttribute("src", "img/icons/proyects.png");
-}
-function proyectsHover(){
-    btnHome.setAttribute("src", "img/icons/home.png");
-    btnAbout.setAttribute("src", "img/icons/About-me.png");
-    btnBook.setAttribute("src", "img/icons/book.png");
-    btnWork.setAttribute("src", "img/icons/work.png");
-    btnProyects.setAttribute("src", "img/icons/proyects-hover.png");
-}
+iconNav.forEach(icon => {
+    icon.addEventListener("click", cambiarIcono);
+});
 
 /*----Banner Video----*/
 let video = document.querySelector(".bannerVideo ");
